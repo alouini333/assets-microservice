d88a9d157ef732ce8f9c42d281b404110845f802
alouini <alouinimedamin@gmail.com>
2023-09-24 13:58:17 +0200

    test: add configuration files

---
+++ jest.unit.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    testRegex: '/tests/unit/.*\\.(test|spec)?\\.(ts|tsx)$',
+    moduleFileExtensions: ['ts', 'js', 'json', 'node']
+  };
