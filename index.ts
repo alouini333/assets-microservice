46279dd28e608a7367db89c43783d3a4f44849ee
alouini <alouinimedamin@gmail.com>
2023-09-24 09:40:40 +0200

    chore: include bodyParser

--- index.ts
+++ index.ts
@@ -2,6 +2,8 @@
 import express from 'express'
 import cors from 'cors'
 import { getDbClient } from './getDbClient'
+import { errorMiddleware } from './src/middlewares/errorMiddleware';
+import assetsRoutes from './src/routes/assetRoutes';
 
 const PORT = 3001
 const HOST = '0.0.0.0'
@@ -9,13 +11,16 @@ const HOST = '0.0.0.0'
 const client = getDbClient()
 
 const app = express()
-app.use(cors())
+const bodyParser = require('body-parser')
+app.use(bodyParser.json());
+app.use(cors());
 
-app.get('/', (req, res) => {
+app.get('/health', (req, res) => {
   res.json({ info: 'App is running!' })
 })
+app.use('/', assetsRoutes);
 
+app.use(errorMiddleware);
 app.listen(PORT, HOST, () => {
-  console.log(`Running on http://${HOST}:${PORT}`)
   console.log(`Connected to database "${client.database}"`)
 })
