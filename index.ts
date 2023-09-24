8c260772e5d4cb1f2b262308e05a936407471d9b
alouini <alouinimedamin@gmail.com>
2023-09-24 13:59:04 +0200

    refactor: entry point

--- index.ts
+++ index.ts
@@ -1,26 +1,11 @@
-'use strict'
-import express from 'express'
-import cors from 'cors'
-import { getDbClient } from './getDbClient'
-import { errorMiddleware } from './src/middlewares/errorMiddleware';
-import assetsRoutes from './src/routes/assetRoutes';
+import app from './app';
+import { getDbClient } from './getDbClient';
 
-const PORT = 3001
-const HOST = '0.0.0.0'
+const PORT = 3001;
+const HOST = '0.0.0.0';
 
-const client = getDbClient()
+const client = getDbClient();
 
-const app = express()
-const bodyParser = require('body-parser')
-app.use(bodyParser.json());
-app.use(cors());
-
-app.get('/health', (req, res) => {
-  res.json({ info: 'App is running!' })
-})
-app.use('/', assetsRoutes);
-
-app.use(errorMiddleware);
 app.listen(PORT, HOST, () => {
-  console.log(`Connected to database "${client.database}"`)
-})
+  console.log(`Connected to database "${client.database}"`);
+});
