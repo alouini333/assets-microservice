73c3a48bc93c31b35bae9c1e16b8791876f0ef97
alouini <alouinimedamin@gmail.com>
2023-09-24 08:05:50 +0200

    feat: add asset controller

---
+++ src/controllers/assetController.ts
@@ -0,0 +1,71 @@
+import { NextFunction, Request, Response } from 'express';
+import * as assetService from '@/services/assetService';
+
+export const getAssets = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    const assets = await assetService.getAllAssetsService();
+    res.json({
+        status: 'success',
+        data: assets
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const getAsset = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    const { id } = req.params;
+    const asset = await assetService.getAssetByIdService(id);
+    res.json({
+        status: 'success',
+        data: asset
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const deleteAsset = async (
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ) => {
+    try {
+        const { id } = req.params;
+        await assetService.deleteAssetService(id);
+        res.json({
+            status: 'success',
+            message: 'Asset deleted with success'
+        });
+    } catch (err) {
+        next(err);
+      }
+  }
+
+
+  export const createAsset = async (
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ) => {
+    try {
+        const { id } = req.params;
+        await assetService.deleteAssetService(id);
+        res.json({
+            status: 'success',
+            message: 'Asset deleted with success'
+        });
+    } catch (err) {
+        next(err);
+      }
+  }
+
