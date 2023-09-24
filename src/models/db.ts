5edb6978d275d91761c80c444897b914b2bcd6bd
alouini <alouinimedamin@gmail.com>
2023-09-24 13:58:47 +0200

    refactor: db to support testing

--- src/models/db.ts
+++ src/models/db.ts
@@ -3,19 +3,25 @@ import knexModule from 'knex';
 
 dotenv.config();
 
-const knex = knexModule({
-  client: 'pg',
-  connection: {
-    host: process.env.DB_HOST,
-    port: parseInt(process.env.DB_PORT || ''),
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-  },
-  pool: {
-    min: 2,
-    max: 10,
-  },
-});
+const isTesting = process.env.NODE_ENV === 'test';
+
+const config = {
+  client: isTesting ? 'sqlite3' : 'pg',
+  connection: isTesting
+    ? {
+        filename: ':memory:',
+      }
+    : {
+        host: process.env.DB_HOST,
+        port: parseInt(process.env.DB_PORT || '', 10),
+        user: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: process.env.DB_NAME,
+      },
+  pool: isTesting ? { min: 1, max: 1 } : { min: 2, max: 10 }, // For SQLite, use a single connection
+  useNullAsDefault: isTesting, // This is needed for SQLite
+};
+
+const knex = knexModule(config);
 
 export { knex };
